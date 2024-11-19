326b1ce370914e38c7cd6521849abb917d2101e1
Railton Araujo <railtonaraujoofc@gmail.com>
2024-11-19 11:20:38 -0300

    first commit

---
+++ Aula05/ex001.js
@@ -0,0 +1,10 @@
+var pais = 'EUA'
+
+console.log(`Vivendo em ${pais}`)
+
+//codição composta
+if (pais == 'Brasil') {
+    console.log('Você e Brasileiro!')
+} else {
+    console.log('Você e Estrangeiro!')
+}
---
+++ Aula05/ex002.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+
+console.log(`Velocidade do seu carro e ${vel}Km/h`)
+
+if (vel > 60) { //condição simples e somente usando o if sem o else.
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de seguraça!`)
